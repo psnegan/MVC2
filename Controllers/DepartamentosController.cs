6baa4eec966972428fb2f334984aeab9d42f4219
3276.343 <3276.343@D9995NE089>
2024-02-06 21:58:17 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/DepartamentosController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC2.Models.ViewModel;
+using System.Collections.Generic;
+
+namespace MVC2.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+
+        {
+            List<Departamento> departamentos = new List<Departamento>();
+            departamentos.Add(new Departamento { Id = 1,Name = "Games"});
+            departamentos.Add(new Departamento { Id = 2, Name = "consoles" });
+            return View();
+        }
+    }
+}
---
+++ Models/ViewModel/Departamento.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC2.Models.ViewModel
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
